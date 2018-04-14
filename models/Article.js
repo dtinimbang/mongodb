adc4df4f2247bc4f02513858595b0e3b8d52b534
don <don.tinimbang@gmail.com>
2018-04-13 20:27:30 -0500

    forgot to commit my changes but will start now

---
+++ models/Article.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var HeadlineSchema = new Schema({
+  title: {
+    type:String,
+    required:true
+  },
+  link: {
+    type:String,
+    required:true
+  },
+  note: {
+      type: Schema.Types.ObjectId,
+      ref: 'Note'
+  },
+  saved: {
+    type:Boolean,
+    default:false
+  },
+});
+
+var Article = mongoose.model('Article', HeadlineSchema);
+module.exports = Article;
